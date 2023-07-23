fe8c3b6010f5e5e7237f0ef954ac08887b7aaf4e
slt_bot <kacheli_qt@mail.ru>
2023-07-23 18:34:28 +0300

    Open window

---
+++ src/main/java/org/example/Arcanoid.java
@@ -0,0 +1,11 @@
+package org.example;
+import javax.swing.*;
+public class Arcanoid {
+    public static void main(String[] args) {
+        JFrame window= new JFrame();
+        window.setTitle("Arcanoid");
+        window.setBounds(10,20,1920,1080);
+        window.setVisible(true);
+
+    }
+}
